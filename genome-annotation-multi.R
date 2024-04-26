71f3414e318419f83b34e4b5f17d7e513b4a786a
Gaurav Sablok <sablokg@gmail.com>
2024-04-26 13:12:30 +0200

    code chunk

---
+++ genome-annotation-multi.R
@@ -0,0 +1,7 @@
+genomemulti <-  function(inputfile, idsfile, outputfile)
+ # Author Gaurav Sablok
+ # Universitat Potsdam
+ # Date 2024-4-26
+ # a multi visualzier genome annotation which will also generate the ribbon plots
+ # from the protein hints.
+ 
